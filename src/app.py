3ab820464d740ef9fc18aa6fa966384d3d87d58d
Peter Driscoll <peteredriscoll@gmail.com>
2024-10-15 21:39:52 -0500

    added framework for app file features

--- src/app.py
+++ src/app.py
@@ -5,6 +5,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#main start page of app (select or make new project)
 @app.route("/")
 def home_page():
     return render_template('index.html')
+
+
+#page for creating new project
+@app.route("/new")
+def home_page():
+    return render_template('new.html')
+
+
+#app page for viewing project
+@app.route("/project")
+def home_page():
+    return render_template('project.html')
