72cea5b446895c66b2fb41a571325da79268472d
Peter Driscoll <peteredriscoll@gmail.com>
2024-10-16 17:01:29 -0500

    added notes to app for future code features

--- src/app.py
+++ src/app.py
@@ -10,14 +10,29 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+    # user will be able to select a project from dropdown box
+    # or user can create new project
+
 
 #page for creating new project
 @app.route("/new")
 def home_page():
     return render_template('new.html')
 
+    #user will need to enter all project information
+    #user can exit and return home
 
-#app page for viewing project
+
+
+#app page for viewing project  MOST CODE WILL NEED TO BE HERE
 @app.route("/project")
 def home_page():
     return render_template('project.html')
+
+    #user can exit the project and return home
+    #placeholder for camera feed will be in the middle left of screen
+    #right side of screen will contain 3 areas
+        #camera set up
+        #station config
+        #data export config
+    
