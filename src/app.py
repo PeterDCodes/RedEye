04de0f792e4c1bd03e5389de17e4c0625d156252
Peter Driscoll <peteredriscoll@gmail.com>
2024-10-17 12:45:34 -0500

    added loop to add class checkbox

--- src/app.py
+++ src/app.py
@@ -38,38 +38,51 @@ ttk.Label(frame1, text="Camera View Frame").grid(column=0, row=0)
 
 
 #frame2
-ttk.Label(frame2, text="Activate").grid(column=0, row=0)
-ttk.Label(frame2, text="Area Name").grid(column=1, row=0)
+#create sub-frames within frame2
+frame2A = ttk.Frame(frame2, padding = 10, border=5, relief="solid")
+frame2B = ttk.Frame(frame2, padding = 10, border=5, relief="solid")
+
+#place sub-frames within frame2
+frame2A.grid(row=0, column=0, sticky="nsew")  # Top-left
+frame2B.grid(row=1, column=0, sticky="nsew")  # Top-right
+
+#column titles
+ttk.Label(frame2A, text="Activate").grid(column=0, row=0)
+ttk.Label(frame2A, text="Area Name").grid(column=1, row=0)
 
 #list of buttons and entrys in frame 2
-ttk.Button(frame2, text='Button1', command=root.destroy, style="Custom.TButton").grid(column=0,row=1)
-ttk.Entry(frame2).grid(column=1,row=1)
-ttk.Button(frame2, text='Button2', command=root.destroy, style="Custom.TButton").grid(column=0, row=2)
-ttk.Entry(frame2).grid(column=1,row=2)
-ttk.Button(frame2, text='Button3', command=root.destroy, style="Custom.TButton").grid(column=0, row=3)
-ttk.Entry(frame2).grid(column=1,row=3)
-ttk.Button(frame2, text='Button4', command=root.destroy, style="Custom.TButton").grid(column=0, row=4)
-ttk.Entry(frame2).grid(column=1,row=4)
-ttk.Button(frame2, text='Button5', command=root.destroy, style="Custom.TButton").grid(column=0, row=5)
-ttk.Entry(frame2).grid(column=1,row=5)
-ttk.Button(frame2, text='Button6', command=root.destroy, style="Custom.TButton").grid(column=0, row=6)
-ttk.Entry(frame2).grid(column=1,row=6)
-ttk.Button(frame2, text='Button7', command=root.destroy, style="Custom.TButton").grid(column=0, row=7)
-ttk.Entry(frame2).grid(column=1,row=7)
-ttk.Button(frame2, text='Button8', command=root.destroy, style="Custom.TButton").grid(column=0, row=8)
-ttk.Entry(frame2).grid(column=1,row=8)
-ttk.Button(frame2, text='Button9', command=root.destroy, style="Custom.TButton").grid(column=0, row=9)
-ttk.Entry(frame2).grid(column=1,row=9)
-ttk.Button(frame2, text='Button10', command=root.destroy, style="Custom.TButton").grid(column=0, row=10)
-ttk.Entry(frame2).grid(column=1,row=10)
+ttk.Button(frame2A, text='Button1', command=root.destroy, style="Custom.TButton").grid(column=0,row=1)
+ttk.Entry(frame2A).grid(column=1,row=1)
+ttk.Button(frame2A, text='Button2', command=root.destroy, style="Custom.TButton").grid(column=0, row=2)
+ttk.Entry(frame2A).grid(column=1,row=2)
+ttk.Button(frame2A, text='Button3', command=root.destroy, style="Custom.TButton").grid(column=0, row=3)
+ttk.Entry(frame2A).grid(column=1,row=3)
+ttk.Button(frame2A, text='Button4', command=root.destroy, style="Custom.TButton").grid(column=0, row=4)
+ttk.Entry(frame2A).grid(column=1,row=4)
+ttk.Button(frame2A, text='Button5', command=root.destroy, style="Custom.TButton").grid(column=0, row=5)
+ttk.Entry(frame2A).grid(column=1,row=5)
+ttk.Button(frame2A, text='Button6', command=root.destroy, style="Custom.TButton").grid(column=0, row=6)
+ttk.Entry(frame2A).grid(column=1,row=6)
+ttk.Button(frame2A, text='Button7', command=root.destroy, style="Custom.TButton").grid(column=0, row=7)
+ttk.Entry(frame2A).grid(column=1,row=7)
+ttk.Button(frame2A, text='Button8', command=root.destroy, style="Custom.TButton").grid(column=0, row=8)
+ttk.Entry(frame2A).grid(column=1,row=8)
+ttk.Button(frame2A, text='Button9', command=root.destroy, style="Custom.TButton").grid(column=0, row=9)
+ttk.Entry(frame2A).grid(column=1,row=9)
+ttk.Button(frame2A, text='Button10', command=root.destroy, style="Custom.TButton").grid(column=0, row=10)
+ttk.Entry(frame2A).grid(column=1,row=10)
 
-ttk.Label(frame2, text='Class Selection').grid(column=0, row=11, columnspan=2)
+#class selection box in frame 2
+ttk.Label(frame2B, text='Class Selection').grid(column=0, row=0, columnspan=2)
+#use a loop to add checkboxed based on list of objects
+objects = ['class1', 'class2', 'class3', 'class4', 'class5']  #temporary list of classes will want to pull from vision model list of classes
+for i, object in enumerate(objects):
+    ttk.Checkbutton(frame2B, text = object). grid(column = 0, row = i+1)
 
 
 #frame3
 ttk.Label(frame3, text="Camera Manager Frame").grid(column=0, row=0)
 ttk.Button(frame3, text="Camera Config Button").grid(column=0, row=1)
-
 
 
 #frame4
