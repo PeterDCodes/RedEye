a8f089d6afaa04ec201971927684cded82d6ebb5
Peter Driscoll <peteredriscoll@gmail.com>
2024-10-17 12:00:50 -0500

    GUI updates of buttons

--- src/app.py
+++ src/app.py
@@ -2,13 +2,18 @@
 from tkinter import *
 from tkinter import ttk
 
-
 #initialize application
 root = Tk()
 root.title('RedEye')
 
+#styles of the application (This is like a .CSS file)
+style = ttk.Style()
+style.theme_use('classic')
+style.configure("Custom.TButton", foreground = "white", background="black", borderwidth=0) 
 
-#create a frame widgets
+
+
+#create root frame widgets
 frame1 = ttk.Frame(root, padding=10, border=5, relief="solid")
 frame2 = ttk.Frame(root, padding=10, border=5, relief="solid")
 frame3 = ttk.Frame(root, padding=10, border=5, relief="solid")
@@ -27,13 +32,7 @@ root.grid_columnconfigure(0, weight=4)
 root.grid_columnconfigure(1, weight=1)
 
 
-#styles
-style = ttk.Style()
-style.theme_use('default') #THEME HERE CAN BE USED LIKE .CSS !!!!!!!!!!!!!!!
-style.configure("Custom.TButton", foreground = "red", background="red")
-
-
-#items in frames-------
+#------items in frames-------
 
 #frame1
 ttk.Label(frame1, text="Camera View Frame").grid(column=0, row=0)
@@ -41,21 +40,31 @@ ttk.Label(frame1, text="Camera View Frame").grid(column=0, row=0)
 
 #frame2
 ttk.Label(frame2, text="Activate").grid(column=0, row=0)
-ttk.Label(frame2, text="Station Name").grid(column=1, row=0)
+ttk.Label(frame2, text="Area Name").grid(column=1, row=0)
 
+#list of buttons and entrys in frame 2
 ttk.Button(frame2, text='Button1', command=root.destroy, style="Custom.TButton").grid(column=0,row=1)
 ttk.Entry(frame2).grid(column=1,row=1)
-
-ttk.Button(frame2, text='Button2', command=root.destroy).grid(column=0, row=2)
+ttk.Button(frame2, text='Button2', command=root.destroy, style="Custom.TButton").grid(column=0, row=2)
 ttk.Entry(frame2).grid(column=1,row=2)
-
-ttk.Button(frame2, text='Button3', command=root.destroy).grid(column=0, row=3)
+ttk.Button(frame2, text='Button3', command=root.destroy, style="Custom.TButton").grid(column=0, row=3)
 ttk.Entry(frame2).grid(column=1,row=3)
-
-ttk.Button(frame2, text='Button4', command=root.destroy).grid(column=0, row=4)
+ttk.Button(frame2, text='Button4', command=root.destroy, style="Custom.TButton").grid(column=0, row=4)
 ttk.Entry(frame2).grid(column=1,row=4)
+ttk.Button(frame2, text='Button5', command=root.destroy, style="Custom.TButton").grid(column=0, row=5)
+ttk.Entry(frame2).grid(column=1,row=5)
+ttk.Button(frame2, text='Button6', command=root.destroy, style="Custom.TButton").grid(column=0, row=6)
+ttk.Entry(frame2).grid(column=1,row=6)
+ttk.Button(frame2, text='Button7', command=root.destroy, style="Custom.TButton").grid(column=0, row=7)
+ttk.Entry(frame2).grid(column=1,row=7)
+ttk.Button(frame2, text='Button8', command=root.destroy, style="Custom.TButton").grid(column=0, row=8)
+ttk.Entry(frame2).grid(column=1,row=8)
+ttk.Button(frame2, text='Button9', command=root.destroy, style="Custom.TButton").grid(column=0, row=9)
+ttk.Entry(frame2).grid(column=1,row=9)
+ttk.Button(frame2, text='Button10', command=root.destroy, style="Custom.TButton").grid(column=0, row=10)
+ttk.Entry(frame2).grid(column=1,row=10)
 
-ttk.Button(frame2, text='Object select').grid(column=0, row=5, columnspan=2)
+ttk.Button(frame2, text='Object select').grid(column=0, row=11, columnspan=2)
 
 
 
@@ -69,8 +78,6 @@ ttk.Button(frame3, text="Camera Config Button").grid(column=0, row=1)
 #frame4
 ttk.Label(frame4, text="Data Export Frame").grid(column=0, row=0)
 ttk.Button(frame4, text="Export as .csv").grid(column=0, row=1)
-
-
 
 
 root.mainloop()
--- src/camera.py
+++ src/camera.py
@@ -8,3 +8,6 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     cv2.imshow('webcam', frame)
+
+
+    
