7b98217f94478b64f58f3f7e48122717c1312ce2
Peter Driscoll <143574249+PeterDCodes@users.noreply.github.com>
2024-11-06 16:23:31 +0800

    camera check fucntion added to check available cameras to use

--- src/helpers.py
+++ src/helpers.py
@@ -14,4 +14,28 @@ def change_model(model):
 from tkinter import filedialog
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files","*.txt*"), ("all files", "*.*")))
-    print('hi')
+
+#function for viewing available camera ports
+import cv2
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 6: # if there are more than 5 non working ports stop the testing. 
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                working_ports.append(dev_port)
+            else:
+                available_ports.append(dev_port)
+        dev_port +=1
+    return working_ports
